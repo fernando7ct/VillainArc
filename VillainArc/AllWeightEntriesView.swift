d5bf41815a30b6f8ff8775e4a727d13335970719
Fernando <fcaudillotafoya@icloud.com>
2024-05-20 22:58:50 -0500

    [Feauture] Added delete all for weight entries

--- VillainArc/AllWeightEntriesView.swift
+++ VillainArc/AllWeightEntriesView.swift
@@ -1,16 +1,11 @@
-//
-//  AllWeightEntriesView.swift
-//  VillainArc
-//
-//  Created by Fernando Caudillo Tafoya on 5/20/24.
-//
-
 import SwiftUI
 import SwiftData
 
 struct AllWeightEntriesView: View {
     @Environment(\.modelContext) private var context
     @Query(sort: \WeightEntry.date, order: .reverse) private var weightEntries: [WeightEntry]
+    @State private var isEditing = false
+    @State private var showDeleteAllAlert = false
     
     private func deleteWeightEntry(offsets: IndexSet) {
         withAnimation {
@@ -21,27 +16,74 @@ struct AllWeightEntriesView: View {
         }
     }
     
+    private func deleteAllEntries() {
+        withAnimation {
+            for entry in weightEntries {
+                DataManager.shared.deleteWeightEntry(weightEntry: entry, context: context)
+            }
+        }
+        isEditing.toggle()
+    }
+    
     var body: some View {
-            List {
-                ForEach(weightEntries, id: \.self) { weightEntry in
-                    HStack {
-                        Image(systemName: "scalemass.fill")
-                            .font(.title2)
-                            .foregroundStyle(.purple)
-                        Text("\(formattedWeight(weightEntry.weight)) lbs")
-                            .fontWeight(.semibold)
-                        Spacer()
-                        VStack(alignment: .trailing) {
-                            Text("\(weightEntry.date.formatted(.dateTime.day().month().year()))")
-                            Text("\(weightEntry.date.formatted(.dateTime.hour().minute()))")
-                        }
-                        .foregroundStyle(.secondary)
-                        .font(.footnote)
+        List {
+            ForEach(weightEntries, id: \.self) { weightEntry in
+                HStack {
+                    Image(systemName: "scalemass.fill")
+                        .font(.title2)
+                        .foregroundStyle(.purple)
+                    Text("\(formattedWeight(weightEntry.weight)) lbs")
+                        .fontWeight(.semibold)
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("\(weightEntry.date.formatted(.dateTime.day().month().year()))")
+                        Text("\(weightEntry.date.formatted(.dateTime.hour().minute()))")
+                    }
+                    .foregroundStyle(.secondary)
+                    .font(.footnote)
+                }
+            }
+            .onDelete(perform: deleteWeightEntry)
+        }
+        .environment(\.editMode, isEditing ? .constant(.active) : .constant(.inactive))
+        .overlay {
+            if weightEntries.isEmpty {
+                ContentUnavailableView("You have no weight entries.", systemImage: "scalemass.fill")
+            }
+        }
+        .navigationTitle("All Weight Entries")
+        .navigationBarBackButtonHidden(isEditing && !weightEntries.isEmpty)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                if isEditing && !weightEntries.isEmpty{
+                    Button(action: {
+                        showDeleteAllAlert = true
+                    }) {
+                        Text("Delete All")
+                            .foregroundColor(.red)
                     }
                 }
-                .onDelete(perform: deleteWeightEntry)
             }
-            .navigationTitle("All Weight Entries")
+            ToolbarItem(placement: .navigationBarTrailing) {
+                if !weightEntries.isEmpty {
+                    Button(action: {
+                        isEditing.toggle()
+                    }) {
+                        Text(isEditing ? "Done" : "Edit")
+                    }
+                }
+            }
+        }
+        .alert(isPresented: $showDeleteAllAlert) {
+            Alert(
+                title: Text("Delete All Entries"),
+                message: Text("Are you sure you want to delete all weight entries?"),
+                primaryButton: .destructive(Text("Delete All")) {
+                    deleteAllEntries()
+                },
+                secondaryButton: .cancel()
+            )
+        }
     }
 }
 
