d761285d29bb24e851bbd96fbc2adfb6da060fad
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-11-12 11:37:03 -0600

    Initial Commit

---
+++ VillainArc/VillainArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  VillainArcApp.swift
+//  VillainArc
+//
+//  Created by Fernando Caudillo Tafoya on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VillainArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
