c0248304850453d1021a56cec810b675f3060c3d
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-11-20 19:20:20 -0600

    Improved muscle filtering, most recently added at the top.

--- VillainArc/Views/Workout/FilteredExerciseListView.swift
+++ VillainArc/Views/Workout/FilteredExerciseListView.swift
@@ -11,7 +11,7 @@ struct FilteredExerciseListView: View {
     var filteredExercises: [Exercise] {
         let cleanText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
 
-        return allExercises.filter { exercise in
+        let filtered = allExercises.filter { exercise in
             let matchesSearch = cleanText.isEmpty ||
                                exercise.name.localizedStandardContains(cleanText) ||
                                exercise.musclesTargeted.contains(where: { $0.rawValue.localizedStandardContains(cleanText) })
@@ -19,6 +19,19 @@ struct FilteredExerciseListView: View {
             let matchesMuscleFilter = muscleFilters.isEmpty || exercise.musclesTargeted.contains(where: { muscleFilters.contains($0) })
 
             return matchesSearch && matchesMuscleFilter
+        }
+
+        return filtered.sorted { first, second in
+            switch (first.lastUsed, second.lastUsed) {
+            case (.some(let date1), .some(let date2)):
+                return date1 > date2
+            case (.some, .none):
+                return true
+            case (.none, .some):
+                return false
+            case (.none, .none):
+                return first.name < second.name
+            }
         }
     }
     
